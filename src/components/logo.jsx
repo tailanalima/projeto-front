2634804737c20343e484009efe10bdc9e05a0e47
tailanalima <tailanalima710@gmail.com>
2026-03-13 19:44:42 -0300

    projeto front

---
+++ src/components/logo.jsx
@@ -0,0 +1,13 @@
+import logo from '../assets/logo-header.svg'; 
+const Logo = () => {
+  return (
+    <img
+      src={logo}
+      alt="Império Doces Logo"
+      width={253}
+      height={44}
+    />
+  );
+};
+
+export default Logo;
